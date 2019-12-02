625d1c0a48733d2610152cc8ed129d1eeda1da36
Administrator <Administrator@PC-20190722FGTI>
2019-12-02 16:54:36 +0800

    66

--- abc/src/abc/Test1.java
+++ abc/src/abc/Test1.java
@@ -6,5 +6,11 @@ public class Test1 {
                 int a=1;
 		System.out.println(a);
 		int b=2;
+		for (int i = 0; i < 10; i++) {
+			a=b+a;
+			b=a+b;
+		}
+		
+		System.out.println(b);
 	}
 }
