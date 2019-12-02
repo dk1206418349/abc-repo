bc70c2e6e46ca7e65b4cc66d83add2e6954c4ab5
DK <58325612+dk1206418349@users.noreply.github.com>
2019-12-02 16:28:32 +0800

    Update Test1.java

--- abc/src/abc/Test1.java
+++ abc/src/abc/Test1.java
@@ -3,7 +3,8 @@ package abc;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("测试");
-               int a=1;
+                int a=1;
 		System.out.println(a);
+		int b=2;
 	}
 }
